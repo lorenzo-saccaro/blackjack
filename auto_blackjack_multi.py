f6c9c59c8622a304795c19536b9611215527e01f
Lorenzo Saccaro <lorenzo.saccaro@hotmail.it>
2020-01-31 23:38:43 +0100

    fixed error in player answer to split

--- auto_blackjack_multi.py
+++ auto_blackjack_multi.py
@@ -112,7 +112,7 @@ def split_possible(player):
 # noinspection DuplicatedCode,DuplicatedCode
 def gameplay(percentage):
 
-    player = Player(1000)
+    player = Player(100000)
     dealer = Dealer()
 
     wins = 0
@@ -318,7 +318,7 @@ def run_gameplay(it):
 
 if __name__ == '__main__':
     from multiprocessing import Pool
-    N = 12  # iterations
+    N = 4  # iterations
     P = 4  # thread
 
     p = Pool(P)
